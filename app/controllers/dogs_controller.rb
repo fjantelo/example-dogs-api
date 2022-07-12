aee598693c15eeb04879f423949f154bd04fd8e0
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-12 15:16:55 -0400

    Adds create method to dogs controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,9 +1,14 @@
 class DogsController < ApplicationController
   def create
-    dog = Dog.new(
-      name: params["name"],
-      age: params["age"],
-      breed: params["breed"],
-    )
+    if authenticate_user
+      dog = Dog.new(
+        name: params["name"],
+        age: params["age"],
+        breed: params["breed"],
+      )
+      dog.save
+    else
+      render json: { error: "Users must be logged in" }
+    end
   end
 end
